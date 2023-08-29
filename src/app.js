38f2cd679b67113609f9852aa74905603a0f4d51
Christa Kelly <46685557+christadkelly@users.noreply.github.com>
2023-08-29 15:45:38 +0000

    fixed to run with node src/app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,18 @@
-/* eslint-disable 
-import "bootstrap";
-import "./style.css";
+const domainNames = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon"];
+const TLD = [".com", ".org"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};*/
-
-window.onload = function() {
-  const pronoun = ["the", "our"];
-  const adj = ["great", "big"];
-  const noun = ["jogger", "racoon"];
-  const TLD = [".com", ".org"];
-
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        for (let m = 0; m < TLD.length; m++) {
-          console.log(`${pronoun[i]}${adj[j]}${noun[k]}${TLD[m]}`);
-        }
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let m = 0; m < TLD.length; m++) {
+        domainNames.push(`${pronoun[i]}${adj[j]}${noun[k]}${TLD[m]}`);
       }
     }
   }
-};
+}
+
+console.log(domainNames);
