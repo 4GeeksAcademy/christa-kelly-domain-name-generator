148dfb3b88e7bff13292c6832026abbf4f2edfbc
Christa Kelly <46685557+christadkelly@users.noreply.github.com>
2023-08-29 18:56:20 +0000

    styled html for web output

--- src/app.js
+++ src/app.js
@@ -1,18 +1,40 @@
-const domainNames = [];
+/*window.onload = function() {
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big"];
+  const noun = ["jogger", "racoon"];
+  const TLD = [".com", ".org"];
 
-const pronoun = ["the", "our"];
-const adj = ["great", "big"];
-const noun = ["jogger", "racoon"];
-const TLD = [".com", ".org"];
+  const domainNames = [];
 
-for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
-    for (let k = 0; k < noun.length; k++) {
-      for (let m = 0; m < TLD.length; m++) {
-        domainNames.push(`${pronoun[i]}${adj[j]}${noun[k]}${TLD[m]}`);
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let m = 0; m < TLD.length; m++) {
+          domainNames.push(`${pronoun[i]}${adj[j]}${noun[k]}${TLD[m]}`);
+        }
       }
     }
   }
-}
+  document.getElementById("Names").innerHTML = domainNames;
+};*/
+import "./index.html";
 
-console.log(domainNames);
+let btn = document.getElementById("btn");
+btn.onclick = function() {
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big"];
+  const noun = ["jogger", "racoon"];
+  const TLD = [".com", ".org"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let m = 0; m < TLD.length; m++) {
+          const newDomains = document.createElement("p");
+          newDomains.innerText = `${pronoun[i]}${adj[j]}${noun[k]}${TLD[m]}`;
+          document.body.appendChild(newDomains);
+        }
+      }
+    }
+  }
+};
