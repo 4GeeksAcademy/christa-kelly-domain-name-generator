c116f3829e15bd4fc09dbd122b6dd51e062802f7
Christa Kelly <46685557+christadkelly@users.noreply.github.com>
2023-08-29 14:27:54 +0000

    domain name generator to console

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable 
 import "bootstrap";
 import "./style.css";
 
@@ -8,4 +8,21 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+};*/
+
+window.onload = function() {
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big"];
+  const noun = ["jogger", "racoon"];
+  const TLD = [".com", ".org"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let m = 0; m < TLD.length; m++) {
+          console.log(`${pronoun[i]}${adj[j]}${noun[k]}${TLD[m]}`);
+        }
+      }
+    }
+  }
 };
